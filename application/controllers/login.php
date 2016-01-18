282f149122859c6feadb5a82cc74c5a3f76ed483
suresh.k <suresh.kamrushi@gmial.com>
2016-01-18 13:02:05 +0530

    reduce code

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,41 +1,21 @@
 <?php
 
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Login extends CI_Controller {
-
-    /**
-     * Index Page for this controller.
-     *
-     * Maps to the following URL
-     * 		http://example.com/index.php/welcome
-     * 	- or -
-     * 		http://example.com/index.php/welcome/index
-     * 	- or -
-     * Since this controller is set as the default controller in
-     * config/routes.php, it's displayed at http://example.com/
-     *
-     * So any other public methods not prefixed with an underscore will
-     * map to /index.php/welcome/<method_name>
-     * @see http://codeigniter.com/user_guide/general/urls.html
-     */
     public function index() {
         $this->load->helper('url');
         $this->load->view('login');
     }
 
     public function signin() {
-
         $email = $_POST['email'];
         $password = $_POST['password'];
         $query = $this->db->query("SELECT * FROM users WHERE email = '$email' && password = md5('$password')");
         $rowcount = $query->num_rows();
-        $json['tpassword'] = 0;
-        $json['temail'] = 0;
+        $json['tpassword'] = $json['temail'] = 0;        
         if ($rowcount == 1) {
             $row = $query->result();
-            $json['tpassword'] = 1;
-            $json['temail'] = 1;
+            $json['tpassword'] = $json['temail'] = 1;            
             $sessionData = array(
                 'email' => $this->input->post('email'),
                 'firstname' => $row[0]->first_name,
